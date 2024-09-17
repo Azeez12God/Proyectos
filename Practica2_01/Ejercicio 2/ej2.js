73f0a8005ce2d1bad8b04b4cd5c3f0f6b29288d1
Alberto <albertolopez.alu@iespacomolla.es>
2024-09-17 20:49:35 +0200

    Siguiendo ejercicio 2

---
+++ Practica2_01/Ejercicio 2/ej2.js
@@ -0,0 +1,9 @@
+let equipoJuan = [89,120,103];
+let equipoMiguel = [116,94,123];
+
+function calcularPuntuacionMediaEquipos(equipo){
+    var puntuacionMedia;
+    for(let i=0; i<equipo.length;i++){
+        puntuacionMedia +=
+    }
+}
