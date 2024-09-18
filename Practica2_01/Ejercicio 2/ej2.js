64555e95dc8d61e798079e0a74ffc64eabd23cf3
Alberto <albertolopez.alu@iespacomolla.es>
2024-09-18 17:27:21 +0200

    Terminado ejercicio 2

--- Practica2_01/Ejercicio 2/ej2.js
+++ Practica2_01/Ejercicio 2/ej2.js
@@ -1,9 +1,37 @@
+// Guardo los puntos de los equipos.
 let equipoJuan = [89,120,103];
 let equipoMiguel = [116,94,123];
+let equipoMaria = [97,134,105];
 
+// Función para calcular la puntuación media de un equipo.
 function calcularPuntuacionMediaEquipos(equipo){
-    var puntuacionMedia;
+    let puntuacionMedia = 0;
     for(let i=0; i<equipo.length;i++){
-        puntuacionMedia +=
+        puntuacionMedia += equipo[i];
+    }
+
+    return puntuacionMedia / equipo.length;
+}
+
+// Función que compara las puntuaciones de los equipos y muestra el ganador.
+function mostrarGanador(equipoJuan, equipoMiguel, equipoMaria, funcionPuntuacion){
+    let puntuacionJuan = funcionPuntuacion(equipoJuan);
+    let puntuacionMiguel = funcionPuntuacion(equipoMiguel);
+    let puntuacionMaria = funcionPuntuacion(equipoMaria);
+
+    if(puntuacionJuan>puntuacionJuan && puntuacionJuan>puntuacionMaria){
+        console.log(`El ganador es el equipo de Juan con ${puntuacionJuan} puntos.`);
+    }
+    else if(puntuacionMiguel>puntuacionJuan && puntuacionMiguel>puntuacionMaria){
+        console.log(`El ganador es el equipo de Miguel con ${puntuacionMiguel} puntos.`);
+    }
+    else if(puntuacionMaria>puntuacionJuan && puntuacionMaria>puntuacionMiguel){
+        console.log(`El ganador es el equipo de María con ${puntuacionMaria} puntos.`);
+    }
+    // Considero que al no haber un equipo con más puntos que los otros no hay ganador.
+    else{
+        console.log(`Ha habido un empate entre algún o algunos equipos. No hay ganador.`);
     }
 }
+
+mostrarGanador(equipoJuan, equipoMiguel, equipoMaria, calcularPuntuacionMediaEquipos);
