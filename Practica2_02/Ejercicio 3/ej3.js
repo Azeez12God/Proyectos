a18f36f539028665db19162cedd1cada5e19e879
Alberto <albertolopez.alu@iespacomolla.es>
2024-09-22 18:53:13 +0200

    Corregir error en el 3 y revisar.

--- Practica2_02/Ejercicio 3/ej3.js
+++ Practica2_02/Ejercicio 3/ej3.js
@@ -13,7 +13,33 @@ function sumaArrays(array1,array2){
     let arraySumas = [];
     if(array1.length==array2.length){
         for(let primerValor=0;primerValor<array1.length;primerValor++){
-
+            for(let segundoValor=array2.length-1;segundoValor>=0;segundoValor--){
+                arraySumas[primerValor] = array1[primerValor] + array2[segundoValor];
+            }
         }
+
+        return arraySumas;
     }
+
+    else
+        return `Ha ocurrido un error: La longitud de los arrays no es la misma.`;
 }
+
+function imprimeArray(arrayImprimir){
+    let arrayFormateado = '';
+    for(let i=0;i<arrayImprimir.length;i++){
+        arrayFormateado += `La posición ${i} del array es: ${arrayImprimir(i)}`;
+        arrayFormateado += `\n`;
+    }
+
+    return arrayFormateado;
+}
+
+function calcular(funcionGenerar,funcionSumar,funcionImprimir){
+    let primerArray = funcionGenerar(5); // Caso de prueba
+    let segundoArray = funcionGenerar(5);
+    let arraySuma = funcionSumar(primerArray,segundoArray);
+    funcionImprimir(arraySuma);
+}
+
+console.log(calcular(generaNumeros,sumaArrays,imprimeArray));
