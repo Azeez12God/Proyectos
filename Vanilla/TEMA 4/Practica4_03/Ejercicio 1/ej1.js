21fbf6da22b9df68b27b3de08a932d9d99e3a104
Alberto <albertolopez.alu@iespacomolla.es>
2024-11-04 18:59:42 +0100

    Empezando rompecabezas

--- Vanilla/TEMA 4/Practica4_03/Ejercicio 1/ej1.js
+++ Vanilla/TEMA 4/Practica4_03/Ejercicio 1/ej1.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { anadirEventoCeldas, anadirEventoColores, borrarTabla, crearTabla } from "../bibliotecas/biblioteca.js";
+import {crearTabla} from "../bibliotecas/biblioteca.js";
 
 window.onload = () => {
     const tablaPintable = document.getElementById("tabla");
@@ -8,15 +8,49 @@ window.onload = () => {
     const columnaColor = document.getElementById("columnaColor");
     const botonBorrar = document.getElementById("boton-borrar");
 
+    // Nombramos una variable que gestiona si se está pintando.
+    let pintando = false;
+
     // Creamos una tabla de 50x60.
     crearTabla(tablaPintable,60,50);
 
     // Añadimos los eventos click a los colores para guardar el color en la fila escondida.
-    anadirEventoColores(tablaColores, columnaColor);
+    tablaColores.addEventListener(
+        "click",
+        (evento)=>{
+            if(evento.target.tagName === "TD")
+                // El nombre de la clase de los td es el nombre de su color.
+                columnaColor.innerHTML = evento.target.className;
+        },
+        false
+    );
 
     // Añadimos los eventos de pintar a las celdas cogiendo el color con la fila escondida.
-    anadirEventoCeldas(tablaPintable,columnaColor);
+    const celdas = tablaPintable.getElementsByTagName('td');
+    for (let i = 0; i < celdas.length; i++) {
+        // Añadimos el evento click a todas las celdas para alternar el estado de pintando.
+        celdas[i].addEventListener('click', () => {
+            pintando ? pintando = false : pintando=true;
+        });
+
+        // Añadimos el evento mousemove para pintar la tabla segun se mueva el ratón.
+        celdas[i].addEventListener('mousemove', (evento) => {
+            // Se pinta la tabla siempre y cuando esté pintando en true y se esté dando click a un TD.
+            if (pintando && evento.target.tagName === "TD") {
+                celdas[i].className = columnaColor.innerHTML;
+            }
+        });
+    }
 
     // Añadimos el evento de poner toda las celdas a blanco (borrar la tabla) al botón.
-    borrarTabla(botonBorrar,tablaPintable);
-}; // Fin del ondload.
+    botonBorrar.addEventListener(
+        "click",
+        ()=>{
+            const celdas = tablaPintable.getElementsByTagName('td');
+            Array.from(celdas).map((celda)=>{
+            celda.className = "white";
+            });
+        },
+        false
+    );
+}; // Fin del onload.
--- Vanilla/TEMA 4/Practica4_03/bibliotecas/biblioteca.js
+++ Vanilla/TEMA 4/Practica4_03/bibliotecas/biblioteca.js
@@ -1,65 +1,15 @@
 "use strict";
 
 // Función para crear una tabla automáticamente a partir de uan estructura HTML ya creada y según las filas y columnas que desees.
-const crearTabla = (tabla, filas,columnas) => {
-    let tablaHTML = '';
+const crearTabla = (tabla, filas, columnas) => {
     for (let i = 0; i < filas; i++) {
-        tablaHTML += '<tr>';
+        let fila = document.createElement("tr");
         for (let j = 0; j < columnas; j++) {
-            tablaHTML += '<td></td>';
+            let celda = document.createElement("td");
+            fila.appendChild(celda);
         }
-        tablaHTML += '</tr>';
-    }
-    tabla.innerHTML = tablaHTML;
-}
-
-// Función para añadir el evento click a los colores.
-const anadirEventoColores = (tablaColores, columnaColor) => {
-    tablaColores.addEventListener(
-        "click",
-        (evento)=>{
-            if(evento.target.tagName === "TD")
-                // El nombre de la clase de los td es el nombre de su color.
-                columnaColor.innerHTML = evento.target.className;
-        },
-        false
-    );
-}
-
-// Función para añadir los eventos para pintar la tabla según el color que contenga la celda escondida.
-const anadirEventoCeldas = (tabla, columnaColor) => {
-    // Nombramos una variable que gestiona si se está pintando.
-    let pintando = false;
-
-    const celdas = tabla.getElementsByTagName('td');
-    for (let i = 0; i < celdas.length; i++) {
-        // Añadimos el evento click a todas las celdas para alternar el estado de pintando.
-        celdas[i].addEventListener('click', () => {
-            pintando ? pintando = false : pintando=true;
-        });
-
-        // Añadimos el evento mousemove para pintar la tabla segun se mueva el ratón.
-        celdas[i].addEventListener('mousemove', (evento) => {
-            // Se pinta la tabla siempre y cuando esté pintando en true y se esté dando click a un TD.
-            if (pintando && evento.target.tagName === "TD") {
-                celdas[i].className = columnaColor.innerHTML;
-            }
-        });
+        tabla.appendChild(fila);
     }
 }
 
-// Función para borrar la tabla (poner todas las celdas en blanco).
-const borrarTabla = (botonBorrar,tabla) => {
-    botonBorrar.addEventListener(
-        "click",
-        ()=>{
-            const celdas = tabla.getElementsByTagName('td');
-            Array.from(celdas).map((celda)=>{
-            celda.className = "white";
-            });
-        },
-        false
-    );
-}
-
-export{crearTabla,anadirEventoColores,anadirEventoCeldas, borrarTabla}
+export{crearTabla}
