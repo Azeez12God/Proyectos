2a50fc877a9fbee01043df2c701551f3198dec25
Alberto <albertolopez.alu@iespacomolla.es>
2024-12-09 19:00:21 +0100

    practicaaaaaaa

--- React/TEMA 5/Practica5_04/src/App.jsx
+++ React/TEMA 5/Practica5_04/src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PeliculaInformación from './componentes/PeliculaInformación.jsx';
+import PeliculaLista from './componentes/PeliculaLista.jsx';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const peliculasIniciales = [];
+  const peliculaSeleccionadaInicial = "";
+  const errorInicial = "";
+  const url = "https://swapi.dev/api/films"; 
+
+  const [peliculas, setPeliculas] = useState(peliculasIniciales);
+  const [peliculaSeleccionada, setPeliculaSeleccionada] = useState(peliculaSeleccionadaInicial);
+  const [error, setError] = useState(errorInicial);
+
+  const manejarPeliculaSeleccionada = (id) => {
+      
+  }
+
+  const traerPeliculas = () => {
+    // Al cargar la página consumimos la URL para que muestre las películas o salga error.
+    fetch(url)
+    .then((respuesta)=>{
+        return respuesta.json();
+    })
+    .then((datos)=>{
+        setPeliculas(datos.results);
+    })
+    .catch((error)=>{
+        setError(error);
+    });
+  }
+
+  useEffect(()=>{
+      traerPeliculas();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+        <div id="contenedor-contenedor">
+          <PeliculaLista peliculas={peliculas}/>
+          <div className="contenedor-informacion">
+            <div className="informacion-titulo" id="informacion-titulo">
+              {peliculaSeleccionada
+              ? <h3>{peliculaSeleccionada.title}</h3>
+              : <h3>No has seleccionado película.</h3>
+              }
+            </div>
+            <div className="informacion-datos" id="informacion-datos">
+              {peliculaSeleccionada
+              ? <PeliculaInformación
+                  sinopsis={peliculaSeleccionada.opening_crawl}
+                  director={peliculaSeleccionada.director}
+                  productor={peliculaSeleccionada.producer}
+                  fechaLanzamiento={peliculaSeleccionada.release_date}
+              />
+              : <p>Clica en una película en la lista de películas.</p>
+              }
+            </div>
+          </div>
+        </div>
+      </>
   )
 }
 
--- React/TEMA 5/Practica5_04/src/componentes/PeliculaLista.jsx
+++ React/TEMA 5/Practica5_04/src/componentes/PeliculaLista.jsx
@@ -2,49 +2,27 @@ import React, { useEffect, useState } from 'react'
 import './PeliculaLista.css';
 import { generarUuidAleatorio } from '../biblioteca/biblioteca.js';
 
-const PeliculaLista = ({seleccionarPelicula}) => {
-    const url = "https://swapi.dev/api/films";
-    const peliculasIniciales = [];
-    const errorInicial = "";
-    const [peliculas, setPeliculas] = useState(peliculasIniciales);
-    const [error, setError] = useState(errorInicial);
-
-    const traerPeliculas = () => {
-        // Al cargar la página consumimos la URL para que muestre las películas o salga error.
-        fetch(url)
-        .then((respuesta)=>{
-            return respuesta.json();
-        })
-        .then((datos)=>{
-            setPeliculas(datos.results);
-        })
-        .catch((error)=>{
-            setError(error);
-        });
-    }
-
-    useEffect(()=>{
-        traerPeliculas();
-    }, [])
-
+const PeliculaLista = ({peliculas, seleccionarPelicula}) => {
     return (
         <>
             <div className="contenedor-peliculas">
                 <h2>Películas Star Wars</h2>
-                {error ? (
-                <p>Error al cargar las películas: {error}</p>
-            ) : (
-                <ul className="peliculas-lista" id="peliculas-lista">
+                <ul className="peliculas-lista" id="peliculas-lista"
+                    onClick={(evento)=>{
+                        if(evento.target.tagName === "LI"){
+                            seleccionarPelicula(evento.target.id);
+                        }
+                    }}
+                >
                     {peliculas.map((pelicula) => (
                         <li 
-                            key={pelicula.episode_id}
-                            onClick={()=>{seleccionarPelicula(pelicula)}}
+                            key={generarUuidAleatorio()}
+                            id={pelicula.episode_id}
                         >
                             Episodio {pelicula.episode_id}: {pelicula.title}
                         </li>
                     ))}
                 </ul>
-            )}
             </div>
         </>
     )
